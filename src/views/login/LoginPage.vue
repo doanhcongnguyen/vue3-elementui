6db9335faa1da8ca41ecfe68d12f54e7c75b8999
doanh.nguyen <doanh.nguyen@dzsi.com>
2022-05-02 21:35:37 +0700

    Validate and submit el-form

--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,15 +1,34 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { setToken } from '@/utils/auth'
 import { useRouter, useRoute } from 'vue-router'
 
 const router = useRouter()
 const route = useRoute()
 
+const formRef = ref()
 const form = ref({
   username: 'anything',
   password: 'anythingelse'
 })
+
+const usernameInput = ref()
+onMounted(() => {
+  usernameInput.value.focus()
+})
+
+const rules = {
+  username: { required: true, message: 'Username is required!', trigger: 'blur' },
+  password: { required: true, message: 'Password is required!', trigger: 'blur' }
+}
+
+async function submitForm() {
+  formRef.value.validate((valid) => {
+    if (valid) {
+      login()
+    }
+  })
+}
 
 function login() {
   const mockToken = 'abc.123'
@@ -21,8 +40,22 @@ function login() {
 <template>
   <div>
     <h1>Login page here..</h1>
-    <div>Username: <input type="text" v-model="form.username" /></div>
-    <div>Password: <input type="password" v-model="form.password" /></div>
-    <div><button @click="login">Login</button></div>
+    <el-form ref="formRef" :model="form" :rules="rules" label-width="15rem" size="large">
+      <el-form-item label="Username" prop="username">
+        <el-input ref="usernameInput" v-model="form.username" class="form-item" />
+      </el-form-item>
+      <el-form-item label="Password" prop="password">
+        <el-input type="password" v-model="form.password" class="form-item" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm">Login</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
+
+<style>
+.form-item {
+  width: 15rem;
+}
+</style>
